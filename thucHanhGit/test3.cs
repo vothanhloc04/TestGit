3a5f68755361b7c9a6c81253d9155afa5a5256cb
lucasvo184 <thanhlocth1tvt@gmail.com>
2024-01-23 10:29:04 +0700

    tao test lan 3

---
+++ thucHanhGit/test3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thucHanhGit
+{
+    internal class test3
+    {
+        //tao lai test
+    }
+}
