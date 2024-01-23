1cb4dbb788161df38b51b4a38ee8b73956235fab
lucasvo184 <thanhlocth1tvt@gmail.com>
2024-01-23 10:17:40 +0700

    tao them file moi ne hehe

---
+++ thucHanhGit/hstest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thucHanhGit
+{
+    internal class hstest
+    {
+        //tao test
+    }
+}
